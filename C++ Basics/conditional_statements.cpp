63b054d3a644483460421580e0582f3a6b3fe71a
Md. Asiful Islam <Saeedasif267@gmail.com>
2024-09-04 21:27:48 +0600

    Create conditional_statements.cpp

---
+++ C++ Basics/conditional_statements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int healthPoints = 50;
+
+    if (healthPoints > 0) {
+        cout << "Keep fighting!" << endl;
+    } else {
+        cout << "Game Over!" << endl;
+    }
+
+    char weapon = 's'; // s = sword, b = bow, m = magic
+
+    switch (weapon) {
+        case
